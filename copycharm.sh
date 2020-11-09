ba4da23319eba18eb15b9337cdc3df95ae341fa0
Sven Hjól <sven.hjol@protonmail.com>
2020-11-09 01:51:12 +0000

    Build for 1.17

--- copycharm.sh
+++ copycharm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cp ../Charm-2.0-fabric/build/libs/charm-fabric-1.16.4-2.1.0-dev.jar ./libs
-cp ../Charm-2.0-fabric/build/libs/charm-fabric-1.16.4-2.1.0-sources-dev.jar ./libs
+cp ../Charm-1.17/build/libs/charm-fabric-1.17-2.1.0-dev.jar ./libs
+cp ../Charm-1.17/build/libs/charm-fabric-1.17-2.1.0-sources-dev.jar ./libs
