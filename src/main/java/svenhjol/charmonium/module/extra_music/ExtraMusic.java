ae1d82c4f2f4568a8eba2e9c795950415964c6b7
Sven Hjól <sven.hjol@protonmail.com>
2021-06-29 01:32:45 +0000

    Remove laggy music condition check, think of another way to do this

--- src/main/java/svenhjol/charmonium/module/extra_music/ExtraMusic.java
+++ src/main/java/svenhjol/charmonium/module/extra_music/ExtraMusic.java
@@ -1,14 +1,12 @@
 package svenhjol.charmonium.module.extra_music;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.Music;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.Level;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import svenhjol.charmonium.Charmonium;
+import svenhjol.charmonium.annotation.Config;
 import svenhjol.charmonium.annotation.Module;
 import svenhjol.charmonium.helper.DimensionHelper;
 import svenhjol.charmonium.helper.RegistryHelper;
@@ -17,7 +15,7 @@ import svenhjol.charmonium.module.CharmoniumModule;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Random;
 import java.util.function.Predicate;
 
 @SuppressWarnings("unused")
@@ -25,11 +23,15 @@ import java.util.function.Predicate;
 public class ExtraMusic extends CharmoniumModule {
     private static final List<MusicCondition> musicConditions = new ArrayList<>();
     public static boolean isEnabled;
+    public static boolean isInRuin;
 
     public static SoundEvent MUSIC_OVERWORLD;
     public static SoundEvent MUSIC_COLD;
     public static SoundEvent MUSIC_NETHER;
     public static SoundEvent MUSIC_RUIN;
+
+    @Config(name = "Play Creative music", description = "If true, the six Creative music tracks may play in survival mode.")
+    public static boolean playCreativeMusic = true;
 
     @Override
     public void register() {
@@ -44,13 +46,21 @@ public class ExtraMusic extends CharmoniumModule {
         // static boolean for mixins to check
         isEnabled = Charmonium.isEnabled("extra_music");
 
-        // overworld music
-        getMusicConditions().add(new MusicCondition(MUSIC_OVERWORLD, 1200, 3600, mc -> {
-            if (mc.player == null || mc.player.level == null) return false;
+        if (playCreativeMusic) {
+            getMusicConditions().add(new MusicCondition(SoundEvents.MUSIC_CREATIVE, 1200, 3600, mc ->
+                mc.player != null
+                    && (!mc.player.isCreative() || !mc.player.isSpectator())
+                    && DimensionHelper.isOverworld(mc.player.level)
+                    && new Random().nextFloat() < 0.25F
+            ));
+        }
 
-            return mc.player.level.random.nextFloat() < 0.08F
-                && DimensionHelper.isOverworld(mc.player.level);
-        }));
+        // overworld music
+        getMusicConditions().add(new MusicCondition(MUSIC_OVERWORLD, 1200, 3600, mc ->
+            mc.player != null
+                && DimensionHelper.isOverworld(mc.player.level)
+                && mc.player.level.random.nextFloat() < 0.08F
+        ));
 
         // cold music, look for player pos in icy biome
         getMusicConditions().add(new MusicCondition(MUSIC_COLD, 1200, 3600, mc ->
@@ -60,45 +70,19 @@ public class ExtraMusic extends CharmoniumModule {
         ));
 
         // nether music, look for player at low position in the nether
-        getMusicConditions().add(new MusicCondition(MUSIC_NETHER, 1200, 3600, mc -> {
-            if (mc.player == null)
-                return false;
-
-            return mc.player.blockPosition().getY() < 48
+        getMusicConditions().add(new MusicCondition(MUSIC_NETHER, 1200, 3600, mc ->
+            mc.player != null
                 && DimensionHelper.isNether(mc.player.level)
-                && mc.player.level.random.nextFloat() < 0.33F;
-        }));
+                && mc.player.blockPosition().getY() < 48
+                && mc.player.level.random.nextFloat() < 0.33F
+        ));
 
-        // ruin music, look around player for stone bricks and cobblestone
-        getMusicConditions().add(new MusicCondition(MUSIC_RUIN, 1200, 3600, mc -> {
-            if (mc.player == null)
-                return false;
-
-            Level level = mc.player.level;
-
-            Optional<BlockPos> optMatch1 = BlockPos.findClosestMatch(mc.player.blockPosition(), 16, 16, pos -> {
-                Block block = level.getBlockState(pos).getBlock();
-                return block == Blocks.STONE_BRICKS
-                    || block == Blocks.CRACKED_STONE_BRICKS
-                    || block == Blocks.MOSSY_STONE_BRICKS
-                    || block == Blocks.DEEPSLATE_BRICKS;
-            });
-
-            Optional<BlockPos> optMatch2 = BlockPos.findClosestMatch(mc.player.blockPosition(), 16, 16, pos -> {
-                Block block = level.getBlockState(pos).getBlock();
-                return block == Blocks.COBBLESTONE
-                    || block == Blocks.MOSSY_COBBLESTONE
-                    || block == Blocks.COBBLED_DEEPSLATE
-                    || block == Blocks.IRON_DOOR
-                    || block == Blocks.IRON_BARS;
-            });
-
-            return mc.player.blockPosition().getY() < 64
-                && DimensionHelper.isOverworld(level)
-                && optMatch1.isPresent()
-                && optMatch2.isPresent()
-                && level.random.nextFloat() < 0.33F;
-        }));
+        // ruin music
+        getMusicConditions().add(new MusicCondition(MUSIC_RUIN, 1200, 3600, mc ->
+            mc.player != null
+                && isInRuin
+                && mc.player.level.random.nextFloat() < 0.33F
+        ));
     }
 
     @Nullable
