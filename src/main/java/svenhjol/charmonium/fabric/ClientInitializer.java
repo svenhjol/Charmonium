250afb760a8b87d146b167c7484e6fac57eb706a
Sven H <sven.hjol@protonmail.com>
2024-05-29 19:49:05 +0000

    Redundant static init

--- src/main/java/svenhjol/charmonium/fabric/ClientInitializer.java
+++ src/main/java/svenhjol/charmonium/fabric/ClientInitializer.java
@@ -5,20 +5,10 @@ import svenhjol.charmonium.Charmonium;
 import svenhjol.charmonium.charmony.client.ClientLoader;
 
 public final class ClientInitializer implements ClientModInitializer {
-    private static boolean initialized = false;
-
     @Override
     public void onInitializeClient() {
-        initCharm();
-    }
-
-    public static void initCharm() {
-        if (initialized) return;
-
         var loader = ClientLoader.create(Charmonium.ID);
         loader.setup(Charmonium.features());
         loader.run();
-
-        initialized = true;
     }
 }
